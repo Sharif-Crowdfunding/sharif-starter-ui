b4ee91ffc2e4ed9b059a954900987ccd665508ae
Hamid <jahad.hamid@gmail.com>
2023-05-27 14:23:08 +0330

    add data

--- src/views/dashboard/Report.js
+++ src/views/dashboard/Report.js
@@ -16,21 +16,22 @@ const Reports = () => {
       <Table variant="simple" colorScheme="facebook" style={{marginTop:"150px"}} dir="rtl">
         <Thead>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
+            <Th>آدرس گیرنده</Th>
+            <Th>نام پروژه</Th>
+            <Th>نام توکن</Th>
+            <Th>قیمت</Th>
+            <Th>وضعیت</Th>
+            <Th>تاریخ</Th>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td isNumeric>25.4</Td>
-          </Tr>
-          <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td isNumeric>30.48</Td>
+            <Td>0xb7e0DBa2a0EB6F9eC0F8a30Bb09afCb0A7baC58F </Td>
+            <Td>طراحی پمپ آب هوشمند </Td>
+            <Td>PMP</Td>
+            <Td>5000000</Td>
+            <Td>در حال انجام</Td>
+            <Td>15:20:02 ,1402/03/06</Td>
           </Tr>
         </Tbody>
       </Table>
