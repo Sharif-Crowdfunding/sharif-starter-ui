137c8174e5d8ba3e585408ad44f1d39c7f94a74b
msadri <marzieh.sadriii@gmail.com>
2023-06-03 18:52:21 +0330

    #1 move codes to individual component

--- src/container/Home.js
+++ src/container/Home.js
@@ -3,37 +3,13 @@ import * as React from "react";
 import Footer from "../views/home/Footer";
 import Hero from "../views/home/Hero";
 import Introduction from "../views/home/Introduction";
-import Auction from "../components/card/Auction";
-import { auctions } from "../components/mock/landingAuctions";
-import { SimpleGrid, useColorModeValue, Stack, Heading } from "@chakra-ui/react";
+import ActionsList from "./AuctionsList";
 
 function Home() {
   return (
     <Box>
       <Hero />
-      <Box bg={useColorModeValue("gray.100", "gray.700")}>
-      <Stack spacing={0} align={"center"}>
-          <Heading fontFamily={"MyShFont"}>مزایده های فعال بازار</Heading>
-        </Stack>
-        <SimpleGrid columns={{ base: 1, md: 5 }} gap="20px" m={5}>
-          {" "}
-          {auctions.map((a) => (
-            <Auction
-              id={a.id}
-              name={a.project.symbol}
-              author={a.project.creator}
-              image={a.project.image}
-              minimumbidpertoken={a.minimum_bid_per_token}
-              saleToken={a.sale_token_num}
-              liked={a.is_liked}
-              endtime={a.end_time}
-              inProgress={!(a.is_ended || a.is_canceled)}
-              isMine
-            />
-          ))}
-        </SimpleGrid>
-      </Box>
-
+      <ActionsList />
       <Introduction />
       <Footer />
     </Box>
