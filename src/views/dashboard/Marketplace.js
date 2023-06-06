72a5d85fe1859710ceb83ca7d53b50c5c68c7cdf
msadri <marzieh.sadriii@gmail.com>
2023-06-06 11:54:04 +0330

    #11 add myActions to dashboard

--- src/views/dashboard/Marketplace.js
+++ src/views/dashboard/Marketplace.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-
 import {
   Box,
   Flex,
@@ -10,11 +9,8 @@ import {
   useColorModeValue,
   useToast,
 } from "@chakra-ui/react";
-
 import Auction from "../../components/card/Auction";
-
 import { useNavigate } from "react-router-dom";
-import Nft3 from "../../assets/img/nfts/Nft3.png";
 import urls from "../../common/urls";
 import { useFetch } from "../../common/useFetch";
 import {
@@ -22,11 +18,10 @@ import {
   MARKET_REFRESH_SUCCESS,
   useMarketReducer,
 } from "../../providers/marketplace";
-import { useAuth } from "../../providers/auth";
+import MyAuctions from "./MyAuctions";
 
 export default function Marketplace() {
   const { state, dispatch } = useMarketReducer();
-  const { user } = useAuth();
   const toast = useToast();
   const navigate = useNavigate();
   const { data, error, loading } = useFetch(urls.auction.market(), "GET");
@@ -55,41 +50,7 @@ export default function Marketplace() {
           flexDirection="column"
           gridArea={{ xl: "1 / 1 / 2 / 3", "2xl": "1 / 1 / 2 / 2" }}
         >
-          <Flex
-            mt="45px"
-            mb="20px"
-            justifyContent="space-between"
-            direction={{ base: "column", md: "row" }}
-            align={{ base: "start", md: "center" }}
-          >
-            <Text color={textColor} fontSize="2xl" ms="24px" fontWeight="700">
-              حراجی های من
-            </Text>
-          </Flex>
-          <SimpleGrid columns={{ base: 1, md: 5 }} gap="20px">
-            {state.isLoading ? (
-              <Spinner size={"lg"} m="20px" />
-            ) : (
-              state.auctions.map((a) =>
-                a.creator === user.data.username ? (
-                  <Auction
-                    id={a.id}
-                    name={a.project.symbol}
-                    author={a.project.creator}
-                    bidders={a.bidders}
-                    image={a.project.image}
-                    minimumbidpertoken={a.minimum_bid_per_token}
-                    saleToken={a.sale_token_num}
-                    onBid={() => navigate("/d/details/" + a.id)}
-                    liked={a.is_liked}
-                    endtime={a.end_time}
-                    inProgress={!(a.is_ended || a.is_canceled)}
-                    isMine
-                  />
-                ) : null
-              )
-            )}
-          </SimpleGrid>
+          <MyAuctions auctions={state.auctions} isLoading={state.isLoading}/>
           <Flex direction="column">
             <Flex
               mt="45px"
